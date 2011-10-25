5c900f5256d5f1eca074dc06258a697280dfbd9d
Marc Juul Christoffersen <marcjc@gmail.com>
2011-10-25 15:40:53 -0700

    migration bug

--- db/migrate/20111008000107_remove_backbones_table.rb
+++ db/migrate/20111008000107_remove_backbones_table.rb
@@ -1,6 +1,6 @@
 class RemoveBackbonesTable < ActiveRecord::Migration
   def up
-    drop_table :backbones
+#    drop_table :backbones
     remove_column :plasmids, :backbone_id
     remove_column :strains, :backbone_id
   end
