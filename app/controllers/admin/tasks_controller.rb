004f21dfa5cc1d076370f935d6e0bc84d4bcab34
Marc Juul Christoffersen <marcjc@gmail.com>
2011-10-25 15:37:01 -0700

    test

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -12,13 +12,12 @@ class Admin::TasksController < ApplicationController
     # ProcessMailer.flowcyte_completed(current_user).deliver
 
     # deliver mail delayed
-    # ProcessMailer.delay.flowcyte_completed(current_user)
+    ProcessMailer.delay.flowcyte_completed(current_user)
 
     # delay method
     # current_user.delay.foowriter
 
     render :text => 'delaying!'
-
   end
 
 end
