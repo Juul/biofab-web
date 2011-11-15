8b1e0432618e3680fb3afb69322684bed5f5c1af
Marc Juul Christoffersen <marcjc@gmail.com>
2011-11-15 12:51:12 -0800

    test

--- app/models/plate_layout.rb
+++ app/models/plate_layout.rb
@@ -7,115 +7,114 @@ class PlateLayout < ActiveRecord::Base
   has_many :plates
 
   def analyze_replicate_dirs(replicate_dirs, user)
-    replicate_dirs.each do |rep_dir|
-      self.analyze_replicate_dir(rep_dir, user)
+    begin
+      cur_rep_dir = ''
+      replicate_dirs.each do |rep_dir|
+        cur_rep_dir = rep_dir
+        self.analyze_replicate_dir(rep_dir, user)
+      end
+      ProcessMailer.flowcyte_completed(user, self.id).deliver
+      
+    rescue Exception => e
+      
+      ProcessMailer.error(user, e, cur_rep_dir).deliver
+
     end
+
   end
 
 
   def analyze_replicate_dir(replicate_dir, user)
-    begin
-      require 'tmpdir'
-      
-      r = RSRuby.instance
-      
-      script_path = File.join(Rails.root, 'r_scripts', 'fcs3_analysis')
-      main_script = File.join(script_path, 'fcs3_analysis.r')
-      
-      out_dir = Dir.mktmpdir('biofab_fcs')
-      
-      dump_file = File.join(Rails.root, 'out.dump')
-      
-      # fluo = 'RED'
-      fluo = 'GRN'
-      #init_gate = 'ellipse'
-      init_gate = 'rectangle'
-      
-      fcs_file_paths = []
-      
-      dir = Dir.new(replicate_dir)
-      dir.each do |fcs_file|
-        next if (fcs_file == '.') || (fcs_file == '..')
-        fcs_file_path = File.join(replicate_dir, fcs_file)
-        next if File.directory?(fcs_file_path)
-        fcs_file_paths << fcs_file_path
-      end
-      
-      r.setwd(script_path)
-      r.source(main_script)
 
-      data_set = Exceptor.call_r_func(r, r.batch, out_dir, fcs_file_paths, :fluo_channel => fluo, :init_gate => init_gate, :verbose => true)
-      
-      # TODO remove this debug code
-      f = File.new(dump_file, 'w+')
-      f.puts(data_set.inspect)
-      f.close
-      
-      plate = Plate.new
-      plate.plate_layout = self
-      plate.name = self.name # TODO what would be a good name?
-
-      data_set.each_pair do |input_file_path, data|
-
-        if !data
-          next
-        end
-        
-        well = PlateWell.new
-        plate.wells << well
-
-        original_fcs_file = DataFile.from_local_file(input_file_path, 'original_fcs_file')
-        well.files << original_fcs_file
-
-        plot_file = DataFile.from_local_file(data['outfile_plot'], 'plot')
-        well.files << plot_file
-
-        well.row, well.column = PlateWell.well_name_to_row_col(data['well_name'])
-        well.replicate = Replicate.new
-        
-        c = Characterization.new_with_type('mean')
-        c.value = data['mean']
-        well.replicate.characterizations << c
-
-        c = Characterization.new_with_type('standard_deviation')
-        c.value = data['standard_deviation']
-        well.replicate.characterizations << c
-
-        c = Characterization.new_with_type('variance')
-        c.value = data['variance']
-        well.replicate.characterizations << c
-
-        c = Characterization.new_with_type('event_count')
-        c.value = data['event_count']
-        well.replicate.characterizations << c
-
-        c = Characterization.new_with_type('cluster_count')
-        c.value = data['cluster_count']
-        well.replicate.characterizations << c
-
-        # TODO unpretty
-        c = Characterization.new_with_type('events')
-        c.value = 0.0
-        c.description = data['events']
-        well.replicate.characterizations << c
-        
-        well.save!
-      end
-
-      plate.save!
-
-      ProcessMailer.flowcyte_completed(user, self.id).deliver
-            
-    rescue Exception => e
-
-#      if(e.message[:r_msg])
-#        @r_msg = e.message[:r_msg]
-#        @r_backtrace = e.message[:r_backtrace]
-#        @ruby_backtrace = e.backtrace
-
-      ProcessMailer.error(user, e, plate.name).deliver
-
+    require 'tmpdir'
+    
+    r = RSRuby.instance
+    
+    script_path = File.join(Rails.root, 'r_scripts', 'fcs3_analysis')
+    main_script = File.join(script_path, 'fcs3_analysis.r')
+    
+    out_dir = Dir.mktmpdir('biofab_fcs')
+    
+    dump_file = File.join(Rails.root, 'out.dump')
+    
+    # fluo = 'RED'
+    fluo = 'GRN'
+    #init_gate = 'ellipse'
+    init_gate = 'rectangle'
+    
+    fcs_file_paths = []
+    
+    dir = Dir.new(replicate_dir)
+    dir.each do |fcs_file|
+      next if (fcs_file == '.') || (fcs_file == '..')
+      fcs_file_path = File.join(replicate_dir, fcs_file)
+      next if File.directory?(fcs_file_path)
+      fcs_file_paths << fcs_file_path
     end
+    
+    r.setwd(script_path)
+    r.source(main_script)
+
+    data_set = Exceptor.call_r_func(r, r.batch, out_dir, fcs_file_paths, :fluo_channel => fluo, :init_gate => init_gate, :verbose => true)
+    
+    # TODO remove this debug code
+    f = File.new(dump_file, 'w+')
+    f.puts(data_set.inspect)
+    f.close
+    
+    plate = Plate.new
+    plate.plate_layout = self
+    plate.name = self.name # TODO what would be a good name?
+
+    data_set.each_pair do |input_file_path, data|
+
+      if !data || !data['error'].blank?
+        next
+      end
+      
+      well = PlateWell.new
+      plate.wells << well
+
+      original_fcs_file = DataFile.from_local_file(input_file_path, 'original_fcs_file')
+      well.files << original_fcs_file
+
+      plot_file = DataFile.from_local_file(data['outfile_plot'], 'plot')
+      well.files << plot_file
+
+      well.row, well.column = PlateWell.well_name_to_row_col(data['well_name'])
+      well.replicate = Replicate.new
+      
+      c = Characterization.new_with_type('mean')
+      c.value = data['mean']
+      well.replicate.characterizations << c
+
+      c = Characterization.new_with_type('standard_deviation')
+      c.value = data['standard_deviation']
+      well.replicate.characterizations << c
+
+      c = Characterization.new_with_type('variance')
+      c.value = data['variance']
+      well.replicate.characterizations << c
+
+      c = Characterization.new_with_type('event_count')
+      c.value = data['event_count']
+      well.replicate.characterizations << c
+
+      c = Characterization.new_with_type('cluster_count')
+      c.value = data['cluster_count']
+      well.replicate.characterizations << c
+
+      # TODO unpretty
+      c = Characterization.new_with_type('events')
+      c.value = 0.0
+      c.description = data['events']
+      well.replicate.characterizations << c
+      
+      well.save!
+    end
+
+    plate.save!
+
   end
 
 
