f3a708b9084f727d02019eed30a04b59f68bf308
Marc Juul Christoffersen <marcjc@gmail.com>
2011-11-16 15:32:59 -0800

    moved fcs analysis r_script to a submodule

--- r_scripts/fcs3_analysis/error.data.r
+++
@@ -1,5 +0,0 @@
-
-
-error.data = function(msg) {
-  return(list("error"=msg))
-}
--- r_scripts/fcs3_analysis/random.string.r
+++
@@ -1,6 +0,0 @@
-
-
-random.string = function(n=1, length=12) {
-  str = paste(sample(c(0:9, letters, LETTERS), length, replace=TRUE), collapse='')
-  return(str)
-}
